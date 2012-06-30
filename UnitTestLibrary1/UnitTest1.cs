28f0811781e756141c511254873a41b2bcfa2629
Raj Aththanayake <rajitha_kba@yahoo.com.au>
2012-06-30 13:13:24 +1000

    new commit

---
+++ UnitTestLibrary1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Moq;
+
+namespace UnitTestLibrary1
+{
+    public interface IFoo
+    {
+        string Bar();
+    }
+
+    public class A
+    {
+        public string M(IFoo foo)
+        {
+            var s = foo.Bar();
+            return s;
+        }
+    }
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var fooMock = new Mock<IFoo>();
+            fooMock.Setup(x => x.Bar()).Returns("foo");
+
+            var a = new A();
+            var r = a.M(fooMock.Object);
+
+            Assert.AreEqual("foo", r);
+
+
+        }
+    }
+}
